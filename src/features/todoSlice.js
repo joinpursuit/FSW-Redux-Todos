9517759e58327c43a24b06179d6adb2ff3658eea
Kevin Wong <kevinwong@pursuit.org>
2020-04-19 11:06:30 -0400

    install toolkit, set up file store, redo index reducer

---
+++ src/features/todoSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { hireFarmer } from "../farmers/farmersSlice";
+
+export const fruitStandSlice = createSlice({
+  name: "fruits",
+  initialState: [],
+  reducers: {
+    addFruit: (state, action) => {
+      state.push(action.payload);
+    },
+    clearFruits: () => [],
+  },
+  extraReducers: {
+    [hireFarmer]: (state) => {
+      state.push("farmer fruit");
+    },
+  },
+});
+
+export const { addFruit, clearFruits } = fruitStandSlice.actions;
+export default fruitStandSlice.reducer;
